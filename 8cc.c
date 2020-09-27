dde38e5d32968ac9de5f4117f399f76490fd0ec3
wbh <1762629204@qq.com>
2020-09-27 14:02:22 +0800

    enable AST

--- 8cc.c
+++ 8cc.c
@@ -1,13 +1,36 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <string.h>
 
-// string 最大长度
 #define BUFLEN 256
 
-// 输出err函数，类似于printf使用
-void error(char *fmt, ...){
+enum {
+    AST_OP_PLUS,
+    AST_OP_MINUS,
+    AST_INT,
+    AST_STR,
+};
+
+typedef struct Ast {
+    int type;
+    union {
+        int ival;
+        char *sval;
+        struct {
+            struct Ast *left;
+            struct Ast *right;
+        };
+    };
+}  Ast;
+
+void error(char *fmt, ...) __attribute__((noreturn));
+void emit_intexpr(Ast *ast);
+Ast *read_string(void);
+Ast *read_expr(void);
+
+void error(char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
     vfprintf(stderr, fmt, args);
@@ -16,111 +39,190 @@ void error(char *fmt, ...){
     exit(1);
 }
 
-// 跳过空格
-void skip_space(void){
+Ast *make_ast_op(int type, Ast *left, Ast *right) {
+    Ast *r = malloc(sizeof(Ast));
+    r->type = type;
+    r->left = left;
+    r->right = right;
+    return r;
+}
+
+Ast *make_ast_int(int val) {
+    Ast *r = malloc(sizeof(Ast));
+    r->type = AST_INT;
+    r->ival = val;
+    return r;
+}
+
+Ast *make_ast_str(char *str) {
+    Ast *r = malloc(sizeof(Ast));
+    r->type = AST_STR;
+    r->sval = str;
+    return r;
+}
+
+void skip_space(void) {
     int c;
-    while((c = getc(stdin)) != EOF) {
-        if((isspace(c)))
+    while ((c = getc(stdin)) != EOF) {
+        if (isspace(c))
             continue;
         ungetc(c, stdin);
-        return ;
+        return;
     }
 }
 
-// 编译数字
-int read_number(int n){
-    int c;
-    // 获取输入
-    while(c = getc((stdin)) != EOF){
-        // 不是数字
-        if(!isdigit(c)){
+Ast *read_number(int n) {
+    for (;;) {
+        int c = getc(stdin);
+        if (!isdigit(c)) {
             ungetc(c, stdin);
-            return n;
+            return make_ast_int(n);
         }
-        // 转换为十进制数字
         n = n * 10 + (c - '0');
     }
 }
 
-void compile_expr2(void) {
-    for(;;) {
-        skip_space();
-        int c = getc(stdin);
-        if (c == EOF) {
-            printf("ret\n");
-            exit(0);
-        }
-        char *op;
-        if (c == '+') op = "add";
-        else if (c == '-') op = "sub";
-        else error("Operator expected, but got '%c'", c);
-        skip_space();
-        c = getc(stdin);
-        if (!isdigit(c))
-            error("Number expected, but got '%c'", c);
-        printf("%s $%d, %%rax\n\t", op, read_number(c - '0'));
-    }
-}
-
-void compile_expr(int n) {
-    n = read_number(n);
-    printf(".text\n\t"
-           ".global intfn\n"
-           "intfn:\n\t"
-           "mov $%d, %%rax\n\t", n);
-    compile_expr2();
-}
-
-// 编译字符串
-void compile_string(void) {
-    char buf[BUFLEN];
-    int i = 0;
-    while(1){
-        int c = getc(stdin);
-        // 字符串未正常结束
-        if(c == EOF){
-            error("Unterminated string");
-        }
-        // 遇到 " 表示字符串终止
-        if(c == '"') break;
-        // 遇到 \ 读取下一个字符
-        if(c == '\\'){
-            c = getc(stdin);
-            // \ 未正常结束
-            if(c == EOF){
-                error("Unterminated \\");
-            }
-        }
-        // 存入buf
-        buf[i++] = c;
-        // string超出最大长度
-        if(i == BUFLEN - 1){
-            error("String is too long");
-        }
-    }
-    // 字符末尾补'\0'表示结束
-    buf[i] = '\0';
-    printf( "\t.data\n"
-                    ".mydata:\n\t"
-                    ".string \"%s\"\n\t"
-                    ".global stringfn\n"
-                    "stringfn:\n\t"
-                    "lea .mydata(%%rip), %%rax\n\t"
-                    "ret\n", buf);
-    exit(0);
-}
-
-void compile(void) {
+Ast *read_prim(void) {
     int c = getc(stdin);
-    if(!isdigit(c))
-        compile_expr(c - '0');
-    else if(c == '"')
-        compile_string();
+    if (isdigit(c))
+        return read_number(c - '0');
+    else if (c == '"')
+        return read_string();
+    else if (c == EOF)
+        error("Unexpected EOF");
+    error("Don't know how to handle '%c'", c);
+}
+
+Ast *read_expr2(Ast *left) {
+    skip_space();
+    int c = getc(stdin);
+    if (c == EOF)
+        return left;
+    int op;
+    if (c == '+') op = AST_OP_PLUS;
+    else if (c == '-') op = AST_OP_MINUS;
+    else error("Operator expected, but got '%c'", c);
+    skip_space();
+    Ast *right = read_prim();
+    return read_expr2(make_ast_op(op, left, right));
+}
+
+Ast *read_string(void) {
+    char *buf = malloc(BUFLEN);
+    int i = 0;
+    for (;;) {
+        int c = getc(stdin);
+        if (c == EOF)
+            error("Unterminated string");
+        if (c == '"')
+            break;
+        if (c == '\\') {
+            c = getc(stdin);
+            if (c == EOF) error("Unterminated \\");
+        }
+        buf[i++] = c;
+        if (i == BUFLEN - 1)
+            error("String too long");
+    }
+    buf[i] = '\0';
+    return make_ast_str(buf);
+}
+
+Ast *read_expr(void) {
+    Ast *left = read_prim();
+    return read_expr2(left);
+}
+
+void print_quote(char *p) {
+    while (*p) {
+        if (*p == '\"' || *p == '\\')
+            printf("\\");
+        printf("%c", *p);
+        p++;
+    }
+}
+
+void emit_string(Ast *ast) {
+    printf("\t.data\n"
+           ".mydata:\n\t"
+           ".string \"");
+    print_quote(ast->sval);
+    printf("\"\n\t"
+           ".text\n\t"
+           ".global stringfn\n"
+           "stringfn:\n\t"
+           "lea .mydata(%%rip), %%rax\n\t"
+           "ret\n");
+    return;
+}
+
+void emit_binop(Ast *ast) {
+    char *op;
+    if (ast->type == AST_OP_PLUS) op = "add";
+    else if (ast->type == AST_OP_MINUS) op = "sub";
+    else error("invalid operand");
+    emit_intexpr(ast->left);
+    printf("mov %%eax, %%ebx\n\t");
+    emit_intexpr(ast->right);
+    printf("%s %%ebx, %%eax\n\t", op);
+}
+
+void ensure_intexpr(Ast *ast) {
+    if (ast->type != AST_OP_PLUS &&
+        ast->type != AST_OP_MINUS &&
+        ast->type != AST_INT)
+        error("integer or binary operator expected");
+}
+
+void emit_intexpr(Ast *ast) {
+    ensure_intexpr(ast);
+    if (ast->type == AST_INT)
+        printf("mov $%d, %%eax\n\t", ast->ival);
     else
-        error("Don't know how to handle '%c'", c);
+        emit_binop(ast);
+}
+
+void print_ast(Ast *ast) {
+    switch (ast->type) {
+        case AST_OP_PLUS:
+            printf("(+ ");
+            goto print_op;
+        case AST_OP_MINUS:
+            printf("(- ");
+        print_op:
+            print_ast(ast->left);
+            printf(" ");
+            print_ast(ast->right);
+            printf(")");
+            break;
+        case AST_INT:
+            printf("%d", ast->ival);
+            break;
+        case AST_STR:
+            print_quote(ast->sval);
+            break;
+        default:
+            error("should not reach here");
+    }
+}
+
+void compile(Ast *ast) {
+    if (ast->type == AST_STR) {
+        emit_string(ast);
+    } else {
+        printf(".text\n\t"
+               ".global intfn\n"
+               "intfn:\n\t");
+        emit_intexpr(ast);
+        printf("ret\n");
+    }
 }
 
 int main(int argc, char **argv) {
-    compile();
+    Ast *ast = read_expr();
+    if (argc > 1 && !strcmp(argv[1], "-a"))
+        print_ast(ast);
+    else
+        compile(ast);
     return 0;
 }
--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
-# 测试是否能编译数字和字符串
 function compile {
   echo "$1" | ./8cc > tmp.s
-  if [ "$?" -ne 0 ] ; then
+  if [ $? -ne 0 ]; then
     echo "Failed to compile $1"
     exit
   fi
@@ -14,50 +13,52 @@ function compile {
   fi
 }
 
-# test函数
-function test {
-  # 函数形参
-  expected="$1"
-  expr="$2"
+function assertequal {
+  if [ "$1" != "$2" ]; then
+    echo "Test failed: $2 expected but got $1"
+    exit
+  fi
+}
 
-  # 把echo "$expr"作为参数传入8cc 并且输入到tmp.s中
-  echo "$expr" | ./8cc > tmp.s
-  # $? 指的是上一个命令的结果
-  if [ ! $? ]; then
-    echo "Failed to compile $expr"
+function testast {
+  result="$(echo "$2" | ./8cc -a)"
+  if [ $? -ne 0 ]; then
+    echo "Failed to compile $1"
     exit
   fi
-  # 编译driver.c和tmp.s到tmp.out文件中
-  gcc -o tmp.out driver.c tmp.s || exit
-  result="`./tmp.out`"
-  # 检查结果
-  if [ "$result" != "$expected" ]; then
-    echo "Test failed: $expected expected but got $result"
-    exit
-  fi
+  assertequal "$result" "$1"
+}
+
+function test {
+  compile "$2"
+  assertequal "$(./tmp.out)" "$1"
 }
 
 function testfail {
   expr="$1"
-  echo "$expr" | 8cc > /dev/null 2>&1
+  echo "$expr" | ./8cc > /dev/null 2>&1
   if [ $? -eq 0 ]; then
-    echo "Should fail to compile, but succeed: &expr"
+    echo "Should fail to compile, but succeded: $expr"
     exit
   fi
 }
 
-# 执行makefile文件
 make -s 8cc
+
+testast '1' '1'
+testast '(+ (- (+ 1 2) 3) 4)' '1+2-3+4'
 
 test 0 0
 test abc '"abc"'
 
-test 3, '1+2'
-test 3, '1 + 2'
-test 10, '1+2+3+4'
+test 3 '1+2'
+test 3 '1 + 2'
+test 10 '1+2+3+4'
+test 4 '1+2-3+4'
 
-testfail '"abc"'
+testfail '"abc'
 testfail '0abc'
 testfail '1+'
+testfail '1+"abc"'
 
 echo "All tests passed"
