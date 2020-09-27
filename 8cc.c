80f280618631682f28c8f53e214a2f91d15c1497
wbh <1762629204@qq.com>
2020-09-27 10:48:18 +0800

    typo

--- 8cc.c
+++ 8cc.c
@@ -16,30 +16,63 @@ void error(char *fmt, ...){
     exit(1);
 }
 
+// 跳过空格
+void skip_space(void){
+    int c;
+    while((c = getc(stdin)) != EOF) {
+        if((isspace(c)))
+            continue;
+        ungetc(c, stdin);
+        return ;
+    }
+}
+
 // 编译数字
-void compile_number(int n){
+int read_number(int n){
     int c;
     // 获取输入
     while(c = getc((stdin)) != EOF){
-        // 空格跳过
-        if(isspace(c))  break;
         // 不是数字
         if(!isdigit(c)){
-            error("Invalid character in number: '%c'", c);
+            ungetc(c, stdin);
+            return n;
         }
         // 转换为十进制数字
         n = n * 10 + (c - '0');
     }
-    // 输出汇编代码
-    printf(  ".text\n\t"
-                    ".global intfn\n"
-                    "intfn:\n\t"
-                    "mov $%d, %%rax\n\t"
-                    "ret\n", n);
+}
+
+void compile_expr2(void) {
+    for(;;) {
+        skip_space();
+        int c = getc(stdin);
+        if (c == EOF) {
+            printf("ret\n");
+            exit(0);
+        }
+        char *op;
+        if (c == '+') op = "add";
+        else if (c == '-') op = "sub";
+        else error("Operator expected, but got '%c'", c);
+        skip_space();
+        c = getc(stdin);
+        if (!isdigit(c))
+            error("Number expected, but got '%c'", c);
+        printf("%s $%d, %%rax\n\t", op, read_number(c - '0'));
+    }
+}
+
+void compile_expr(int n) {
+    n = read_number(n);
+    printf(".text\n\t"
+           ".global intfn\n"
+           "intfn:\n\t"
+           "mov $%d, %%rax\n\t", n);
+    compile_expr2();
 }
 
 // 编译字符串
-void compile_string(void){
+void compile_string(void) {
     char buf[BUFLEN];
     int i = 0;
     while(1){
@@ -74,18 +107,20 @@ void compile_string(void){
                     "stringfn:\n\t"
                     "lea .mydata(%%rip), %%rax\n\t"
                     "ret\n", buf);
+    exit(0);
 }
 
-void compile(void){
+void compile(void) {
     int c = getc(stdin);
     if(!isdigit(c))
-        return compile_number(c - '0');
-    if(c == '"')
-        return compile_string();
-    error("Don't know how to handle '%c'", c);
+        compile_expr(c - '0');
+    else if(c == '"')
+        compile_string();
+    else
+        error("Don't know how to handle '%c'", c);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     compile();
     return 0;
 }
